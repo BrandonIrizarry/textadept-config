b3318902c438e917972379d15c3ce02254f3ebde
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2020-08-19 09:23:52 -0400

    Added terminal opening binding, and lua pattern find module.

--- modules/init_gui.lua
+++ modules/init_gui.lua
@@ -1,1 +1,7 @@
 view:set_theme("light", {font="Iosevka Fixed Slab", size=20})
+
+-- Open a terminal where the current buffer's file resides.
+keys["ctrl+t"] = function ()
+	local directory = buffer.filename:match("(.-)[^/]*$")
+	os.spawn("desktop-defaults-run -t", directory)
+end
