8ed20c2cc76858cf0a68d82f0b2c3a7fabc885c2
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2020-10-11 20:11:58 -0400

    Eliminated Lua-pattern search.
    For one thing, search-wrap doesn't work.

--- init.lua
+++ init.lua
@@ -3,9 +3,11 @@ require "ta_as_commit_editor"
 require("textredux").hijack()
 
  -- Enable Lua patterns, and set as the default search mode.
+ --[[
 local tlp = require("lua-pattern-find")
 tlp.toggle_lua_patterns()
 ui.find.regex = true -- doubles as the Lua pattern option
+]]
 
 if not CURSES then
 	require "init_gui"
